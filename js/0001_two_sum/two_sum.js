2e89f9a3caf0a8b3a39d31e23fc5719f58f517c9
Ravi Damarla <ravi.damarla@gmail.com>
2022-02-12 17:03:59 +0000

    added implementation and tests

--- js/0001_two_sum/two_sum.js
+++ js/0001_two_sum/two_sum.js
@@ -7,6 +7,13 @@
  you may not use the same element twice.
 
  You can return the answer in any order.
+
+ Constraints:
+ -  2 <= nums.length <= 104
+ - -109 <= nums[i] <= 109
+ - -109 <= target <= 109
+ - Only one valid answer exists.
+
  * =====
  */
 
@@ -16,7 +23,32 @@ module.exports = {
    * @param {number} target
    * @return {number[]}
    */
-  twoSum: function(nums, target) {
+  twoSum: function( nums, target ) {
+
+    for ( let index = 0; index < nums.length; index++ ) {
+      for ( let index2 = 0; index2 < nums.length; index2++ ) {
+        if ( index !== index2 && ( nums[ index ] + nums[ index2 ] === target ) ) {
+          return [ index, index2 ];
+        }
+      }
+    }
+
+  },
+
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  twoSumOpt: function( nums, target ) {
+
+    for ( let index = 0; index < nums.length; index++ ) {
+      for ( let index2 = 0; index2 < nums.length; index2++ ) {
+        if ( index !== index2 && ( nums[ index ] + nums[ index2 ] === target ) ) {
+          return [ index, index2 ];
+        }
+      }
+    }
 
   }
 };

6281089645d1a4a2792ac44f4c3344e7e73e0a80
Ravi Damarla <ravi.damarla@gmail.com>
2022-02-14 12:31:42 +0000

    added test cases while dev

--- js/0002_add_two_numbers/app.js
+++ js/0002_add_two_numbers/app.js
@@ -1,11 +1,15 @@
-import ListNode from './list_node';
-
+let listNode = require('./list_node');
 let addNum = require('./add_two_numbers');
 
-ListNode l1D3 = new ListNode(4);
-ListNode l1D2 = new ListNode(6, l1D3);
-ListNode l1 = new ListNode(5, l1D2);
+const l1 = listNode.buildList( [2, 4, 3] );
+const l2 = listNode.buildList( [5, 6, 4] );
 
-ListNode l2D3 = new ListNode(8);
-ListNode l2D2 = new ListNode(0, l2D3);
-ListNode l2 = new ListNode(7, l2D2);
+listNode.printList( l1 );
+listNode.printList( l2 );
+
+const listSum = addNum.addTwoNumbers( l1, l2 );
+listNode.printList( listSum, 'list sum:' );
+
+const l3 = listNode.buildList( [ 9, 9, 9, 9, 9, 9, 9 ] );
+const l4 = listNode.buildList( [ 9, 9, 9, 9 ] );
+const sum1 = listNode.toArray( addNum.addTwoNumbers( l3, l4 ) );

c3c83e0d21cd518c4097bcf37c63c03696afe2a1
Ravi Damarla <ravi.damarla@gmail.com>
2022-02-14 12:31:08 +0000

    added implementation of addTwoNumbers

--- js/0002_add_two_numbers/add_two_numbers.js
+++ js/0002_add_two_numbers/add_two_numbers.js
@@ -11,7 +11,7 @@
  * =====
  */
 
-import ListNode from './list_node';
+let listNode = require( './list_node' );
 
 /**
  * @param {ListNode} l1
@@ -19,6 +19,44 @@ import ListNode from './list_node';
  * @return {ListNode}
  */
 module.exports = {
-  addTwoNumbers: function ( l1, l2 ) {
+  addTwoNumbers: function( l1, l2 ) {
+
+    let carry = 0;
+    let hasCarry = false;
+
+    let sumValues = [];
+
+    do {
+      let val1 = ( ( l1 ) ? l1.val : 0 );
+      let val2 = ( ( l2 ) ? l2.val : 0 );
+      let sum = val1 + val2;
+
+      // If there is a carry from the previous operation, add it to the sum.
+      if ( hasCarry ) {
+        sum += 1;
+        // The carry has been used up; reset the flag.
+        hasCarry = false;
+      }
+
+      // If there are two digits, set the carry flag.
+      if ( sum > 9 ) {
+        sum = sum % 10;
+        hasCarry = true;
+      }
+
+      // Save the digit.
+      sumValues.push( sum );
+
+      // Get the next nodes.
+      l1 = ( l1 ) ? l1.next : null;
+      l2 = ( l2 ) ? l2.next : null;
+    } while ( l1 || l2 );
+
+    // If there is a pending carry, add a new digit.
+    if ( hasCarry ) {
+      sumValues.push( 1 );
+    }
+
+    return listNode.buildList( sumValues );
   }
 };

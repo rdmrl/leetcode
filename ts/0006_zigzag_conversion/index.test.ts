1ac8fba0bbc51e375ce526ef3c098b691c2acf1f
Ravi Damarla <ravi.damarla@gmail.com>
2022-02-17 19:16:14 +0000

    added tests

--- ts/0006_zigzag_conversion/index.test.ts
+++ ts/0006_zigzag_conversion/index.test.ts
@@ -1,7 +1,20 @@
-import zigzag from "./zigzag";
+import convert from "./zigzag";
 
 describe("hello", () => {
   it("should return true", () => {
     expect(true).toBe(true);
   });
 });
+
+describe("zigzag", () => {
+  it("should return 'PAHNAPLSIIGYIR' when the input is 'PAYPALISHIRING' with 3 rows", () => {
+    expect(convert('PAYPALISHIRING', 3)).toBe('PAHNAPLSIIGYIR');
+  });
+});
+
+describe("zigzag", () => {
+  it("should return 'PINALSIGYAHRPI' when the input is 'PAYPALISHIRING' with 4 rows", () => {
+    expect(convert('PAYPALISHIRING', 4)).toBe('PINALSIGYAHRPI');
+  });
+});
+

0fcd418cfa138943760f5efedbd9f0cc8a2512bc
Ravi Damarla <ravi.damarla@gmail.com>
2022-03-01 11:51:26 +0000

    updates to pass all tests

--- ts/0088_merge_sorted_array/app.ts
+++ ts/0088_merge_sorted_array/app.ts
@@ -4,10 +4,12 @@ let nums1 = [1, 2, 3, 0, 0, 0];
 let nums2 = [2, 5, 6];
 merge(nums1, 3, nums2, 3);
 
-/*
-let nums1: number[] = [1];
-let nums2: number[] = [];
-merge(nums1, 1, nums2, 0);
-*/
-
 console.log(nums1);
+
+console.log('---------------------');
+
+let nums3: number[] = [1];
+let nums4: number[] = [];
+merge(nums3, 1, nums4, 0);
+
+console.log(nums3);
--- ts/0088_merge_sorted_array/merge.ts
+++ ts/0088_merge_sorted_array/merge.ts
@@ -3,15 +3,21 @@
  */
 export default function merge(nums1: number[], m: number, nums2: number[], n: number): void {
 
+  console.log(nums1);
+  console.log(nums2);
+
   // Start at the last valid element in nums1.
   let ix = Math.max(0, m - 1);
   const offsetIx = m - 1;
 
+  console.log('ix:', ix, 'offsetIx:', offsetIx);
+
   // Start at the last element in nums2.
   for(let jx = n - 1; jx >= 0; jx--) {
 
-    const ix1 = Math.min(ix, offsetIx + jx + 1);
-    console.log(ix, nums1[ix], jx, nums2[jx]);
+    // const ix1 = Math.min(ix, offsetIx + jx + 1);
+    const ix1 = offsetIx + jx + 1;
+    console.log('ix:', ix, nums1[ix], 'jx:', jx, nums2[jx], 'ix1:', ix1);
     if(nums1[ix] < nums2[jx]) {
       nums1[ix1] = nums2[jx];
     } else {
